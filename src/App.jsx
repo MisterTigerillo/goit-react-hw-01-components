3cb9c6a8b13bc93ea816ea642a2c4daa55e540fc
Dmytro Gobosh <88391310+MisterTigerillo@users.noreply.github.com>
2022-10-12 18:43:52 +0300

    +misc

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { GlobalStyle } from "./GlobalStyle";
-import Profile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/Statistics";
-import FriendList from "./components/FriendList/FriendList";
+// import { GlobalStyle } from "./GlobalStyle";
+// import Profile from "./components/Profile/Profile";
+// import Statistics from "./components/Statistics/Statistics";
+// import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import World from "./components/Timeline/Timeline";
 
-import user from "./json/user.json";
-import data from "./json/data.json";
-import friends from "./json/friends.json";
+// import user from "./json/user.json";
+// import data from "./json/data.json";
+// import friends from "./json/friends.json";
 import transactions from "./json/transactions.json";
 
 export default function App() {
   return (
     <div className="App">
-      <GlobalStyle />
+      {/* <GlobalStyle />
       <Profile
         username={user.username}
         tag={user.tag}
@@ -21,8 +22,9 @@ export default function App() {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
+      <FriendList friends={friends} /> */}
       <TransactionHistory items={transactions} />
+      <World />
     </div>
   );
 }
